7f0df40beceee099a514be0c286f269e094a13c2
Jony <jony@core>
2021-06-26 17:42:01 -0300

    tercer commit

--- app.js
+++ app.js
@@ -11,3 +11,25 @@ function enviar_formulario(event){
    }
   
 }
+
+document.onmouseover = ver;
+            function ver(e){
+                var x, y, x1, x2, y1, y2;
+                fact = 800/400;
+                opp=100;
+                if(e = null) e = window.event;
+
+                x = e.clientX;
+                y = e.clientY;
+                x1 = - opp+(x)*fact;
+                y1 = - opp+(y)*fact;
+                x2 = + opp+(x)*fact;
+                y2 = + opp+(y)*fact;
+
+                document.images.grande.style.display ="inline";
+                document.images.grande.style.left = (x) * (1-fact);
+                document.images.grande.style.top = (y) * (1-fact);
+                document.images.grande.style.clip ="rect("+y1+"px," +x2+ "px,"+y2+ "px," +x1+ "px)";
+                    
+                
+            }
